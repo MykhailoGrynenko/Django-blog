4032d8559061ebddb3c76c24a93e7b14f1dc1749
zerog <banehallovvv1@gmail.com>
2021-06-21 04:39:56 +0300

    another try

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -153,9 +153,9 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False  # to not allow overwrite files, if users upload files with the same names
 AWS_DEFAULT_ACL = None
 
-AWS_S3_REGION_NAME = 'us-west-2'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_S3_REGION_NAME = 'us-west-2'
+# AWS_S3_SIGNATURE_VERSION = 's3v4'
+#
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())

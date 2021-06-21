953a626391eb9daf2b0789759690895d1dc42832
zerog <banehallovvv1@gmail.com>
2021-06-21 03:45:25 +0300

    Added settings.py changes.

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gr$eaq1vwi5#qw1!3!9u1vbx^6&gh=$(bc84c4_=)yb^k1u!re'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myamazingdjangoblog.heroku.app.com']
 
 
 # Application definition
